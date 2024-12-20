7a6ccfe70ac65a06566cb9d48f840767dcec61f6
aminelaarif <laarifamine5569@gmail.com>
2024-12-20 23:03:51 +0100

    navabr logo changed and new menu animatio

--- src/Components/Home/NavBar.jsx
+++ src/Components/Home/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo2.png";
 function NavBar() {
 
   const [isScrolled, setIsScrolled] = useState(false);
